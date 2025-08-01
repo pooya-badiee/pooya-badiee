d7f28faa93c3343162c8d75b1bd2253241225eff
pooya <pooya.badiee76@gmail.com>
2025-08-01 14:59:35 -0700

    add table

--- src/home-screen/index.tsx
+++ src/home-screen/index.tsx
@@ -4,13 +4,8 @@ function HomeScreen() {
   return (
     <main className={classes.canvasOwner}>
       <div className={classes.canvas}>
-        <img
-          src="/images/background.jpg"
-          width={3615}
-          height={2259}
-          className={classes.canvasBackground}
-          alt="background"
-        />
+        <img src="/images/background.jpg" width={3615} height={2259} className={classes.background} alt="background" />
+        <img src="/images/table.png" width={3615} height={2259} className={classes.table} alt="table" />
       </div>
     </main>
   )
