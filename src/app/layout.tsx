fefd649e98cc1c69e41e7a2ec580568025e01113
pooya <pooya.badiee76@gmail.com>
2025-08-23 09:01:50 -0700

    add a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,16 @@ import '../globals.scss'
 export const metadata: Metadata = {
   title: 'Pooya Badiee - Full Stack Developer',
   description: 'TODO',
+  icons: [
+    {
+      url: '/assets/favicon-light.svg',
+      media: '(prefers-color-scheme: dark)',
+    },
+    {
+      url: '/assets/favicon-dark.svg',
+      media: '(prefers-color-scheme: light)',
+    },
+  ],
 }
 
 // Since it is a graphical portfolio, we want to disable zooming
