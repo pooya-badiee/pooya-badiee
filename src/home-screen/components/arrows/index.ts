638e7755efd5ddacf290237cf523b5eb1a5299ed
pooya <pooya.badiee76@gmail.com>
2025-08-23 09:09:02 -0700

    add new arrows

--- src/home-screen/components/arrows/index.ts
+++
@@ -1,2 +0,0 @@
-export { PointToLaptopArrow } from './point-to-laptop-arrow'
-export { PointToPhoneArrow } from './point-to-phone-arrow'
--- src/home-screen/index.tsx
+++ src/home-screen/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import clsx from 'clsx'
-import { PointToLaptopArrow, PointToPhoneArrow } from './components/arrows'
 import HomeScreenImage from './components/home-screen-image'
 import classes from './styles.module.scss'
 import useAreImagesLoaded from './use-are-images-loaded'
@@ -16,13 +15,11 @@ function HomeScreen() {
         <HomeScreenImage path="/assets/table.png" alt="Table" className={classes.layer} />
         <HomeScreenImage path="/assets/phone.png" alt="Phone" className={clsx(classes.layer, classes.phoneLayer)} />
         <HomeScreenImage path="/assets/laptop.png" alt="Laptop" className={clsx(classes.layer, classes.laptopLayer)} />
+        <HomeScreenImage path="/assets/arrows.png" alt="Arrows" className={classes.layer} />
         <HomeScreenImage path="/assets/glasses.png" alt="Glasses" className={classes.layer} />
         {/* Selections */}
         <button type="button" aria-label="Select Laptop" className={classes.selectLaptop} />
         <button type="button" aria-label="Select Phone" className={classes.selectPhone} />
-        {/* arrows */}
-        <PointToLaptopArrow className={classes.arrow} />
-        <PointToPhoneArrow className={classes.arrow} />
       </div>
     </main>
   )
